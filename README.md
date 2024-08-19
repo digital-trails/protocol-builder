37ef0663608fdba6ebc96f3327feb2a8bbbe702e
mrucker <rucker.mark@gmail.com>
2024-08-19 16:02:50 -0400

    Improved the documentation in the readme as well as the HTML root file.

--- README.md
+++ README.md
@@ -1,8 +1,11 @@
 # Protocol Builder
 A web app to build protocols
 
-# Instructions for Static Builds
-https://docs.expo.dev/router/reference/static-rendering/
-
 # Instructions about directory structure
 https://docs.expo.dev/router/reference/src-directory/
+
+# Instructions about publishing websites (e.g., static or single)
+https://docs.expo.dev/distribution/publishing-websites/
+
+# Instructions for configuring static publishing
+https://docs.expo.dev/router/reference/static-rendering/
--- app/+html.tsx
+++ app/+html.tsx
@@ -4,6 +4,12 @@ import { type PropsWithChildren } from 'react';
 /**
  * This file is web-only and used to configure the root HTML for every web page during static rendering.
  * The contents of this function only run in Node.js environments and do not have access to the DOM or browser APIs.
+ * mrucker:
+ *    for more info https://docs.expo.dev/router/reference/static-rendering/#root-html
+ *    as far as I can tell there is a default html boilerplate which is what is here. So
+ *    If this file were to be removed there will be minimal change in the static HTML output.
+ *    Changes to this file can be seen when running `npx expo start --web` as we as when
+ *    running `npx expo export -p web; npx serve dist` so long as app.json>expo.web.output='static'.
  */
 export default function Root({ children }: PropsWithChildren) {
   return (
